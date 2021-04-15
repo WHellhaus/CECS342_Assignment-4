d4a759ab59faeddbe64001f6765b05a2185f89b8
WHellhaus <wolfgang.hellicksons@gmail.com>
2021-04-15 14:25:35 -0700

    generator code

--- test/Program.cs
+++ test/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Xml.Linq;
 
 namespace test
@@ -8,22 +9,37 @@ namespace test
     {
         public static IEnumerable<string> EnumerateFilesRecursively(string path)
         {
+            foreach (var file in Directory.GetFiles(path))
+            {
+                yield return file;
+            }
 
+            foreach (var directory in Directory.GetDirectories(path))
+            {
+                foreach (var file in EnumerateFilesRecursively(directory))
+                    yield return file;
+            }
         }
 
-        public static string FormatByteSize(long byteSize)
-        {
+        //public static string FormatByteSize(long byteSize)
+        //{
 
-        }
+        //}
 
-        public static XDocument CreateReport(IEnumerable<string> files)
-        {
+        //public static XDocument CreateReport(IEnumerable<string> files)
+        //{
 
-        }
+        //}
 
         static void Main(string[] args)
         {
-            
+            var appDataDir = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            //var tempDir = Path.Combine(appDataDir, "Documents");
+            Console.WriteLine(appDataDir);
+            Console.WriteLine("hello");
+            var files = EnumerateFilesRecursively(appDataDir.ToString() + "/Documents/School/CECS_342/Lab_04/");
+            //Console.WriteLine(appDataDir.ToString() + "/School/");
+            foreach (var f in files) Console.WriteLine(f);
         }
     }
 }
