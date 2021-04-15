b02bdad0ed01fe5fba7a71f91548b04c9dd25029
WHellhaus <wolfgang.hellicksons@gmail.com>
2021-04-15 12:32:03 -0700

    first commit

---
+++ test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace test
+{
+    class Program
+    {
+        public static IEnumerable<string> EnumerateFilesRecursively(string path)
+        {
+
+        }
+
+        public static string FormatByteSize(long byteSize)
+        {
+
+        }
+
+        public static XDocument CreateReport(IEnumerable<string> files)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
